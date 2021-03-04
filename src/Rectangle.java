e9f71f231fad49123c1d9a955d8a276b4946ad75
Amar Ahamed <66819012+amarahamed@users.noreply.github.com>
2021-03-04 16:28:56 +0530

    Initial commit - Added dir tests and generated a test for Rectangle class

---
+++ src/Rectangle.java
@@ -0,0 +1,42 @@
+public class Rectangle
+{
+    private int length, width;
+
+    public Rectangle(int length, int width)
+    {
+        this.length = length;
+        this.width = width;
+    }
+
+    /*
+        Perimeter of the rectangle 2*Length + 2*W
+     */
+    public int getPerimeter()
+    {
+        return 0;
+    }
+    /*
+        Area = L * W
+     */
+    public int getArea()
+    {
+        return 0;
+    }
+
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+}
---
+++ tests/RectangleTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RectangleTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getPerimeter() {
+    }
+
+    @Test
+    void getArea() {
+    }
+
+    @Test
+    void setLength() {
+    }
+
+    @Test
+    void setWidth() {
+    }
+}
