92e5da9ef05f5175600d24a98ab4172518b7c533
Amar Ahamed <66819012+amarahamed@users.noreply.github.com>
2021-03-04 20:14:58 +0530

    Created circle class and added tests for Circle class

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,34 @@
 public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        setRadius(radius);
+    }
+
+    public double getArea ()
+    {
+        // formula 1/2 * PI * radius
+        return 0.5*Math.PI*radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if(radius > 0)
+        {
+            this.radius = radius;
+        }
+        else
+        {
+            throw new IllegalArgumentException("Radius can not be zero! should be > 0");
+        }
+    }
+
+    public String toString ()
+    {
+        // " .1f " only display 1 digit after decimal point
+        return String.format("Circle with radius %.1f", radius);
+    }
 }
---
+++ tests/CircleTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircleTest {
+
+    private Circle circle;
+
+    @BeforeEach
+    void setUp() {
+        circle = new Circle(10);
+    }
+
+    @Test
+    void getArea() {
+    }
+
+    @Test
+    void setRadius() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
