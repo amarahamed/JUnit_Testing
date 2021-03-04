ef3e58b4570718ecf56f02405622c1cc39a04ab3
Amar Ahamed <66819012+amarahamed@users.noreply.github.com>
2021-03-04 17:30:18 +0530

    added some tests for the rectangle class

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Rectangle rectangle = new Rectangle(10,20);
+        /*
+            rectangle.setWidth(50);
+            System.out.println("Expects 10: " + rectangle.getLength());
+            System.out.println("Expects 20: " + rectangle.getWidth());
+        */
+    }
+}
--- tests/RectangleTest.java
+++ tests/RectangleTest.java
@@ -5,20 +5,66 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RectangleTest {
 
+    private Rectangle rectangle;
+    private Rectangle square;
+
+    // this runs before every test
     @BeforeEach
-    void setUp() {
+    void setUp()
+    {
+        // before each test the object is rebuild
+        rectangle = new Rectangle(10,20);
+        square = new Rectangle(20,20);
     }
 
     @Test
-    void getPerimeter() {
+    void getPerimeter()
+    {
+        // Rectangle with L 10 W 20
+        // Perimeter = 2 * L + 2 * W = 60
+        /*
+        * Expected for rectangle is 60
+        * 1st param - expected output
+        * 2nd param - actual output
+        */
+        assertEquals(60,rectangle.getPerimeter());
     }
 
     @Test
-    void getArea() {
+    void getArea()
+    {
+        // Rectangle L 10 W 20
+        // Area = L * W = 200
+        /*
+         * Expected for rectangle is 200
+         */
+        assertEquals(200, rectangle.getArea());
     }
 
     @Test
-    void setLength() {
+    void setLength()
+    {
+        // lowest value allowed for length -> greater thn 0
+        // max value allowed for length -> invalid
+        rectangle.setLength(50);
+        assertEquals(50, rectangle.getLength());
+    }
+    @Test
+    void setLengthInvalidLowValue()
+    {
+        // lowest value allowed for length -> greater thn 0
+        try
+        {
+            // if length is 0
+            rectangle.setLength(0);  // this value is not allowed so its an IllegalArgumentException
+            fail("Length of 0 is not allowed! Trigger an exception");
+        }
+        catch (IllegalArgumentException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        rectangle.setLength(50);
+        assertEquals(50, rectangle.getLength());
     }
 
     @Test
